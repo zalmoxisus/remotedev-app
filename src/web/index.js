d001931a93c6f342ebfed4258634c0145398c643
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-06 11:51:35 +0200

    Render DevTools with remote store

--- src/web/index.js
+++ src/web/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import DevTools from '../app/containers/DevTools';
+import createRemoteStore from '../app/store/createRemoteStore.js';
+const store = createRemoteStore();
 
 render(
-  <div>Hi!</div>,
+  <DevTools store={store} />,
   document.getElementById('root')
 );
