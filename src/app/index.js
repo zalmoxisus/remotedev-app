c83d2fcfc0edd95b2771d8d265694b4c644b5a04
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-11 14:44:34 +0200

    Use React Component for the main container

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './styles';
 import DevTools from './containers/DevTools';
 import createRemoteStore from './store/createRemoteStore';
 import ButtonBar from './components/ButtonBar';
 
-export default ({ socketOptions }) => (
-  <div style={styles.container}>
-    <DevTools store={createRemoteStore(socketOptions)} />
-    <ButtonBar/>
-  </div>
-);
+export default class extends Component {
+  static propTypes = {
+    socketOptions: PropTypes.shape({
+      hostname: PropTypes.string,
+      port: PropTypes.number,
+      autoReconnect: PropTypes.bool
+    })
+  };
+
+  componentWillMount() {
+    this.store = createRemoteStore(this.props.socketOptions);
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <DevTools store={this.store} />
+        <ButtonBar/>
+      </div>
+    );
+  }
+}
