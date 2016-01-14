a9c92f80dd8380bd1e62ef666a444ba6dc876f09
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-14 17:14:04 +0200

    Create `INIT` action

--- src/app/store/updateState.js
+++ src/app/store/updateState.js
@@ -1,11 +1,19 @@
 import parseJSON from '../utils/parseJSON';
 
-function recompute(previousLiftedState, storeState, action, nextActionId) {
+function recompute(previousLiftedState, storeState, action, nextActionId = 1) {
   const actionId = nextActionId - 1;
   const liftedState = { ...previousLiftedState };
   liftedState.stagedActionIds = [...liftedState.stagedActionIds, actionId];
   liftedState.actionsById = { ...liftedState.actionsById };
-  liftedState.actionsById[actionId] = action;
+  if (action.type === 'PERFORM_ACTION') {
+    liftedState.actionsById[actionId] = action;
+  } else {
+    liftedState.actionsById[actionId] = {
+      action: action.action || action,
+      timestamp: action.timestamp,
+      type: 'PERFORM_ACTION'
+    };
+  }
   liftedState.nextActionId = nextActionId;
   liftedState.computedStates = [...liftedState.computedStates, { state: storeState }];
   liftedState.currentStateIndex++;
@@ -16,15 +24,32 @@ export default function updateState(store, request) {
   const payload = parseJSON(request.payload);
   if (!payload) return null;
 
+  let newState;
+  let action = {};
+  if (request.action) action = parseJSON(request.action) || {};
+
   switch (request.type) {
+    case 'INIT':
+      newState = recompute(
+        store.liftedStore.getInitialState(),
+        payload,
+        { action: { type: '@@INIT' }, timestamp: action.timestamp }
+      );
+      break;
     case 'ACTION':
-      const newState = recompute(store.liftedStore.getState(), payload, parseJSON(request.action), request.nextActionId);
-      store.liftedStore.setState(newState);
-      return newState;
+      newState = recompute(
+        store.liftedStore.getState(),
+        payload,
+        action,
+        request.nextActionId
+      );
+      break;
     case 'STATE':
-      store.liftedStore.setState(payload);
-      return payload;
+      newState = payload;
+      break;
     default:
       return null;
   }
+  store.liftedStore.setState(newState);
+  return newState;
 }
