a3b62dad773b5581e4262f4767fc74cecdc9e39e
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-30 17:08:28 +0200

    Add `How to use` button

--- src/app/components/ButtonBar.js
+++ src/app/components/ButtonBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import SettingsIcon from 'react-icons/lib/md/settings';
+import HelpIcon from 'react-icons/lib/fa/lightbulb-o';
 import Button from './Button';
 import styles from '../styles';
 import Settings from './Settings';
 
 export default ({ openModal, closeModal, saveSettings, socketOptions }) => (
   <div style={styles.buttonBar}>
+    <Button
+      Icon={HelpIcon}
+      onClick={() => { window.open('https://github.com/zalmoxisus/remote-redux-devtools'); }}
+    >How to use</Button>
     <Button
       Icon={SettingsIcon}
       onClick={() => openModal(
