8a2ec857611a074987d5914faa875e8463eb8b49
Zalmoxisus <zalmoxisus@gmail.com>
2016-04-20 16:53:28 +0300

    Refactor ButtonBar component

--- src/app/components/ButtonBar.js
+++ src/app/components/ButtonBar.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import SettingsIcon from 'react-icons/lib/md/settings';
 import HelpIcon from 'react-icons/lib/fa/lightbulb-o';
 import Button from './Button';
 import styles from '../styles';
 import Settings from './Settings';
 
-export default ({ openModal, closeModal, saveSettings, socketOptions }) => (
-  <div style={styles.buttonBar}>
-    <Button
-      Icon={HelpIcon}
-      onClick={() => { window.open('https://github.com/zalmoxisus/remote-redux-devtools'); }}
-    >How to use</Button>
-    <Button
-      Icon={SettingsIcon}
-      onClick={() => openModal(
-        <Settings closeModal={closeModal}
-          saveSettings={saveSettings} socketOptions={socketOptions}
-        />
-      )}
-    >Settings</Button>
-  </div>
-);
+export default class ButtonBar extends Component {
+  static propTypes = {
+    openModal: PropTypes.func.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    saveSettings: PropTypes.func.isRequired,
+    socketOptions: PropTypes.func.object
+  };
+
+  constructor() {
+    super();
+    this.openSettings = this.openSettings.bind(this);
+  }
+
+  openHelp() {
+    window.open('https://github.com/zalmoxisus/remote-redux-devtools');
+  }
+
+  openSettings() {
+    this.props.openModal(
+      <Settings
+        closeModal={this.props.closeModal}
+        saveSettings={this.props.saveSettings}
+        socketOptions={this.props.socketOptions}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div style={styles.buttonBar}>
+        <Button Icon={SettingsIcon} onClick={this.openSettings}>Settings</Button>
+        <Button Icon={HelpIcon} onClick={this.openHelp}>How to use</Button>
+      </div>
+    );
+  }
+}
