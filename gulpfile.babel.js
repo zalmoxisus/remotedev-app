4c7c09b5c0531cc971200fbd0a97123eb6b23e2d
Zalmoxisus <zalmoxisus@gmail.com>
2016-05-02 16:06:21 +0300

    Use `webpack-hot-middleware`

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,6 @@ requireDir('./gulp/tasks');
 
 gulp.task('default', ['webpack-dev-server', 'views:dev', 'copy:dev']);
 gulp.task('build:web', ['webpack:build:web', 'views:build:web']);
-gulp.task('build:electron', ['webpack:build:electron', 'views:build:electron']);
+gulp.task('build:electron', ['webpack:build:electron', 'views:build:electron', 'copy:build:electron']);
 gulp.task('build:app', ['webpack:build:app', 'views:build:app', 'copy:build:app']);
 gulp.task('build:umd', ['webpack:build:umd']);
