1c5170bf38e237d0839cf1e91dceef4c06a8651c
Zalmoxisus <zalmoxisus@gmail.com>
2016-05-14 17:37:04 +0300

    Use `material-ui`

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { blueGrey300 } from 'material-ui/styles/colors';
 import App from './containers/App';
+
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: blueGrey300
+  }
+});
 
 export default class Root extends Component {
   render() {
-    return <App />;
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
+    );
   }
 }
