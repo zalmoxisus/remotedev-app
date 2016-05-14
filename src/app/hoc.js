f92ede4b674acf3d670e2ba581919426378d1908
Zalmoxisus <zalmoxisus@gmail.com>
2016-05-14 22:04:39 +0300

    Theming

--- src/app/hoc.js
+++ src/app/hoc.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { blueGrey300 } from 'material-ui/styles/colors';
+import lightTheme from './themes/light';
+import darkTheme from './themes/dark';
 
 injectTapEventPlugin();
 
-const muiTheme = getMuiTheme({
-  palette: {
-    accent1Color: blueGrey300
-  }
-});
+const enhance = (ComposedComponent, isLightTheme) => class extends Component {
+  theme = getMuiTheme(isLightTheme ? lightTheme : darkTheme);
 
-
-const enhance = ComposedComponent => class extends Component {
   render() {
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
+      <MuiThemeProvider muiTheme={this.theme}>
         <ComposedComponent {...this.props} />
       </MuiThemeProvider>
     );
---
+++ src/app/themes/light.js
@@ -0,0 +1,29 @@
+import {
+  cyan500, cyan700,
+  blueGrey300,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/styles/colors';
+import { fade } from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
+
+export default {
+  spacing,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: cyan500,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: blueGrey300,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack
+  }
+};
