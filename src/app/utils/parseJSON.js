b892d5c65f6fb6c03aefd4aa82bad859b70a8bf9
Zalmoxisus <zalmoxisus@gmail.com>
2016-10-14 14:53:02 +0300

    Circular references inside states and action's payloads are stripped now, in order to make monitoring faster and stable

--- src/app/utils/parseJSON.js
+++ src/app/utils/parseJSON.js
@@ -1,9 +1,9 @@
-import { parse } from 'jsan';
+// import { parse } from 'jsan';
 
 export default function parseJSON(data) {
   if (typeof data !== 'string') return data;
   try {
-    return parse(data);
+    return JSON.parse(data);
   } catch (e) {
     if (process.env.NODE_ENV !== 'production') console.error(data + 'is not a valid JSON', e);
     return undefined;
