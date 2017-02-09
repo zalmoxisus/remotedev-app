d70625a88a55dd34871a27a5146cf5306bfe9eb4
Zalmoxisus <zalmoxisus@gmail.com>
2017-02-09 21:46:01 +0200

    Add tabs for Settings

--- src/app/components/Settings/index.js
+++ src/app/components/Settings/index.js
@@ -1,3 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+import { Tabs } from 'remotedev-ui';
 import Connection from './Connection';
 
-export default Connection;
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.tabs = [
+      { name: 'Connection', component: Connection }
+    ];
+    this.state = { selected: 'Connection' };
+  }
+
+  handleSelect = selected => {
+    this.setState({ selected });
+  };
+
+  render() {
+    return <Tabs tabs={this.tabs} selected={this.state.selected} onClick={this.handleSelect} />;
+  }
+}
+
+export default Settings;
