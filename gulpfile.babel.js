c92605597116a932eca35b3c9e163de72568f094
Zalmoxisus <zalmoxisus@gmail.com>
2017-02-13 18:58:35 +0200

    Remove unnecessary files

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import requireDir from 'require-dir';
-requireDir('./gulp/tasks');
-
-gulp.task('default', ['webpack-dev-server', 'views:dev', 'copy:dev']);
-gulp.task('build:web', ['webpack:build:web', 'views:build:web']);
-gulp.task('build:electron', ['webpack:build:electron', 'views:build:electron', 'copy:build:electron']);
-gulp.task('build:app', ['webpack:build:app', 'views:build:app', 'copy:build:app']);
-gulp.task('build:umd', ['webpack:build:umd']);
