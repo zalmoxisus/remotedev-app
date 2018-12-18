d47b0bb3c533c359e0515879ba685c53b05e2406
Mark Erikson <mark@isquaredsoftware.com>
2018-12-18 12:32:15 -0500

    Show stack traces (#43)

--- src/app/containers/monitors/InspectorWrapper/index.js
+++ src/app/containers/monitors/InspectorWrapper/index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import InspectorMonitor from 'remotedev-inspector-monitor';
+import StackTraceTab from 'redux-devtools-trace-monitor';
 import { DATA_TYPE_KEY } from '../../../constants/dataTypes';
 import SubTabs from './SubTabs';
 import TestTab from './TestTab';
@@ -22,7 +23,7 @@ class InspectorWrapper extends Component {
     const { lib, ...rest } = this.props;
     let tabs;
     if (lib === 'redux') {
-      tabs = () => [...DEFAULT_TABS, { name: 'Test', component: TestTab }];
+      tabs = () => [...DEFAULT_TABS, { name: 'Trace', component: StackTraceTab }, { name: 'Test', component: TestTab }];
     } else {
       tabs = () => DEFAULT_TABS;
     }
